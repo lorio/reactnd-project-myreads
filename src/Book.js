16c3a745ff27f8c9afe6315049445ea623117847
lorio <lori@loriortiz.com>
2018-09-19 14:59:28 -0400

    Refactor onChange

--- src/Book.js
+++ src/Book.js
@@ -18,7 +18,9 @@ class Book extends Component {
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
           <div className="book-shelf-changer">
             <select value={shelf} 
-              onChange={updateShelf}
+              onChange={(event) => updateShelf(
+                book, event.target.value
+                )}
               >
                 <option value="move" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
